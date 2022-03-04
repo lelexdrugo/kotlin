d00e4cd39eab9cf85599050e8b0256745bab504a
GABRIELE SCAFFIDI MILITONE s288200 <s288200@studenti.polito.it>
2022-03-04 17:30:57 +0100

    feat: first exercise solved

--- exercises/practice/matrix/src/main/kotlin/Matrix.kt
+++ exercises/practice/matrix/src/main/kotlin/Matrix.kt
@@ -1,10 +1,27 @@
+fun main(){
+    val m =Matrix("1 2\n3 4")
+    val l:List<Int> = m.row(2)
+    println(l)
+}
+
 class Matrix(private val matrixAsString: String) {
 
     fun column(colNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
-    }
+        val internalIndex = colNr-1
+        val rows: List<String> = matrixAsString.lines()
+        //val rows: List<String> = matrixAsString.split("\n")
+        val selectedColumns: MutableList<Int> = mutableListOf()
+        rows.forEach{
+            val temp: Int = it.trim().split("\\s+".toRegex()).elementAt(internalIndex).toInt()
+            selectedColumns.add(temp)}
+        return selectedColumns
 
+    }
+    //non gestisco la richiesta di una riga oltre la dimensione
     fun row(rowNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        val internalIndex = rowNr-1
+        val rows: List<String> = matrixAsString.lines()
+        val selectedRow: List<Int> = rows[internalIndex].split("\\s+".toRegex()).map{it.toInt()}
+        return selectedRow
     }
 }
