5672b343627a6182e3572cb765b2509932a9fee7
GABRIELE SCAFFIDI MILITONE s288200 <s288200@studenti.polito.it>
2022-03-04 18:22:03 +0100

    feat: completed second exercise. With comment

--- exercises/practice/flatten-array/src/main/kotlin/Flattener.kt
+++ exercises/practice/flatten-array/src/main/kotlin/Flattener.kt
@@ -1,5 +1,16 @@
 object Flattener {
     fun flatten(source: Collection<Any?>): List<Any> {
-        TODO("Implement the function to complete the task")
+        //molto interessante: non è semplicemente un filter{not null}: in questo modo la collection ritornata sarebbe comunque di tipo Any?
+        //Internamente questo fa un filter e dopo un map a->!!a così da poter avere come valore di ritorno una collection di Any senza ?
+        val step1 = source.filterNotNull()
+        //Ricorda: la flatmap vuole una lambda che ritorni oggetti iterable (liste ecc) e lui si occupa semplicemente di concatenare tali valori di ritorno
+        //delle chiamate alla lambda che fa per ogni elemento dell'iterable che l'ha usata
+        val step2 = step1.flatMap {
+            if(it is Collection<Any?>)
+                flatten(it)
+            else
+                listOf(it)
+        }
+        return step2
     }
 }
