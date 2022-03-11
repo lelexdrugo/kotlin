3949090cf603a76609b9a01ee0c678f858c7f555
lelexdrugo <gabrielescaffidi@gmail.com>
2022-03-11 17:02:01 +0100

    feat: WA2 third exercise working

--- exercises/practice/word-count/src/main/kotlin/WordCount.kt
+++ exercises/practice/word-count/src/main/kotlin/WordCount.kt
@@ -1,6 +1,12 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        TODO("Implement this function to complete the task")
+        //val words = phrase.split(("\\s+").toRegex()).map{it.lowercase()}
+        val words = phrase.split(("[^a-zA-Z1-9']+").toRegex()).map{it.lowercase().trim('\'')}.filter{it.isNotEmpty()}
+        //splitta quando incontra qualcosa che NON sia carattere, numero, o carattere ' (anche ripetuto)
+
+        val myMap = words.groupBy { it }.mapValues { it.value.size }
+        println(myMap)
+        return myMap
     }
 }
