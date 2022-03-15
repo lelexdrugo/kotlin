2ed1cf3bdb1456d66b6e01304f6c71df42607f73
lelexdrugo <gabrielescaffidi@gmail.com>
2022-03-15 10:10:02 +0100

    feat: MAD second exercise completed

--- exercises/practice/bank-account/src/main/kotlin/BankAccount.kt
+++ exercises/practice/bank-account/src/main/kotlin/BankAccount.kt
@@ -1,11 +1,24 @@
 class BankAccount {
-    // TODO: implement read access to 'balance'
+    //Per garantire la thread-safeness posso usare o l'annotation synchronized o il metodo synchronized this: ogni esecuzione di ciò che è scritto tra le graffe (o nella funzione annotata) sarà thread safe
+    private var accessible = true
+    var balance = 0
+        get() {
+            synchronized(this) {
+            check(accessible)
+            return field
+            //if(accessible)  return field else throw IllegalStateException("Non più accessibile")
+            }
+        }
+        @Synchronized
+        private set
 
+
+    @Synchronized
     fun adjustBalance(amount: Long){
-        TODO("Implement the function to complete the task")
+        if(accessible)  balance+=amount.toInt() else throw IllegalStateException("Non più accessibile")
     }
 
     fun close() {
-        TODO("Implement the function to complete the task")
+        accessible = false
     }
 }
